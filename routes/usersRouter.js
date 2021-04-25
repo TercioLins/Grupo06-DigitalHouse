b1884402a068db736ba6b7ac6b1d4c4f701451e7
Ganhatos <ganhatos@gmail.com>
2021-04-25 17:15:13 -0300

    implementado middleware

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const usersController = require("../controllers/usersController");
+const ValidateUser = require('../middlewares/ValidateUser');
 
 /* GET users listing. */
 router.get("/", usersController.index);
-router.post("/", usersController.create);
+router.post("/", ValidateUser, usersController.create);
 router.put("/:id", usersController.update);
 router.delete("/:id", usersController.delete);
 router.get("/:id", usersController.find);
