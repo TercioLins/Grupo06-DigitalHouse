ad9258b2d4727913215289aa221cab944e44b1b5
Ganhatos <ganhatos@gmail.com>
2021-04-25 18:40:56 -0300

    Adicionado middleware de alterar cadastro

--- middlewares/ValidateUserRegister.js
+++ middlewares/ValidateUserRegister.js
@@ -22,6 +22,12 @@ module.exports = async (req, res, next) => {
     } else if (!emailValidate.test(email)) {
         return res.status(400).json({ erro: "Email inválido!" });
         
+    } else if (!phoneNumberValidator(phone_number)) {
+        return res.status(400).json({ erro: "Telefone inválido!" });
+        
+    } else if (password.length < 8 || password.length > 15) {
+        return res.status(400).json({ erro: "Senha deve ser entre 8 e 15 caracteres." });
+        
     } else {
         next();
     }
@@ -44,5 +50,31 @@ const cpfFormatValidate = strCPF => {
 
     if ((rest == 10) || (rest == 11))  rest = 0;
     if (rest != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+}
+
+const phoneNumberValidator = telefone => {
+    telefone = telefone.replace(/\D/g, '');
+
+    if (!(telefone.length >= 10 && telefone.length <= 11)) return false;
+
+    if (telefone.length == 11 && parseInt(telefone.substring(2, 3)) != 9) return false;
+
+    for (var n = 0; n < 10; n++) {
+        if (telefone == new Array(11).join(n) || telefone == new Array(12).join(n)) return false;
+    }
+    var codigosDDD = [11, 12, 13, 14, 15, 16, 17, 18, 19,
+        21, 22, 24, 27, 28, 31, 32, 33, 34,
+        35, 37, 38, 41, 42, 43, 44, 45, 46,
+        47, 48, 49, 51, 53, 54, 55, 61, 62,
+        64, 63, 65, 66, 67, 68, 69, 71, 73,
+        74, 75, 77, 79, 81, 82, 83, 84, 85,
+        86, 87, 88, 89, 91, 92, 93, 94, 95,
+        96, 97, 98, 99];
+    if (codigosDDD.indexOf(parseInt(telefone.substring(0, 2))) == -1) return false;
+
+    if (new Date().getFullYear() < 2017) return true;
+    if (telefone.length == 10 && [2, 3, 4, 5, 7].indexOf(parseInt(telefone.substring(2, 3))) == -1) return false;
+
     return true;
 }
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const usersController = require("../controllers/usersController");
-const ValidateUser = require('../middlewares/ValidateUser');
+const ValidateUserRegister = require('../middlewares/ValidateUserRegister');
+const ValidateUserUpdate = require('../middlewares/ValidateUserUpdate');
 
 /* GET users listing. */
 router.get("/", usersController.index);
-router.post("/", ValidateUser, usersController.create);
-router.put("/:id", usersController.update);
+router.post("/", ValidateUserRegister, usersController.create);
+router.put("/:id", ValidateUserUpdate, usersController.update);
 router.delete("/:id", usersController.delete);
 router.get("/:id", usersController.find);
 
