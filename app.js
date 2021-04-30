fe8289728e0356efa62dfea2b899ef8cedcc60ed
Ganhatos <ganhatos@gmail.com>
2021-04-30 16:52:02 -0300

    alterando modal agendamento

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-<<<<<<< HEAD
-
-let indexRouter = require('./routes/index');
-let userRouter = require('./routes/userRouter');
-=======
 const session = require('express-session');
 
 let indexRouter = require('./routes/index');
@@ -16,7 +10,6 @@ let availableHoursRouter = require('./routes/availableHoursRouter');
 let schedulesRouter = require('./routes/schedulesRouter');
 let usersRouter = require('./routes/usersRouter');
 let addressesRouter = require('./routes/addressesRouter');
->>>>>>> 5077fa2bc7f43e93bd85f7420b51f572d2c30cbc
 
 let app = express();
 
@@ -36,15 +29,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-<<<<<<< HEAD
-app.use('/users', userRouter);
-=======
 app.use('/users', usersRouter);
 app.use("/hours", availableHoursRouter);
 app.use("/schedules", schedulesRouter);
 app.use('/addresses', addressesRouter);
 
->>>>>>> 5077fa2bc7f43e93bd85f7420b51f572d2c30cbc
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -63,7 +52,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-=======
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
@@ -118,4 +106,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
->>>>>>> 2bd724290c54e46e59d8acc2ff98b227117ad531
--- routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userCotroller = require('../controllers/userController');
-
-/* GET users listing. */
-router.get('/', userCotroller.index);
-
-module.exports = router;
