80e01ce0ff8acd32a5b86f8ecb3e7b6c0dc3e990
Ganhatos <ganhatos@gmail.com>
2021-05-02 19:11:59 -0300

    movendo modal de login e registro para index

--- app.js
+++ app.js
@@ -37,60 +37,6 @@ app.use('/addresses', addressesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-module.exports = app;
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-const session = require('express-session');
-
-let indexRouter = require('./routes/index');
-let availableHoursRouter = require('./routes/availableHoursRouter');
-let schedulesRouter = require('./routes/schedulesRouter');
-let usersRouter = require('./routes/usersRouter');
-let addressesRouter = require('./routes/addressesRouter');
-
-let app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(session({
-  secret: "instagram-avanade",
-  saveUninitialized: true,
-  resave: true
-}));
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/hours", availableHoursRouter);
-app.use("/schedules", schedulesRouter);
-app.use('/addresses', addressesRouter);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
   next(createError(404));
 });
 
